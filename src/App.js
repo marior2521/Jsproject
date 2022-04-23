868461d30074c9cfa903e3555e5602b341a51f3d
marior2521 <74110086+marior2521@users.noreply.github.com>
2022-04-22 21:56:03 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,22 +8,7 @@ import AddFavourites from './components/AddFavourites';
 import RemoveFavourites from './components/RemoveFavourites';
 
 const App = () => {
-	const [movies, setMovies] = useState([{
-    "Title": "Guardians of the Galaxy Vol. 2",
-    "Year": "2017",
-    "Rated": "PG-13",
-    "Released": "05 May 2017",
-    "Runtime": "136 min",
-    "Genre": "Action, Adventure, Comedy",
-    "Director": "James Gunn",
-    "Writer": "James Gunn, Dan Abnett, Andy Lanning",
-    "Actors": "Chris Pratt, Zoe Saldana, Dave Bautista",
-    "Plot": "The Guardians struggle to keep together as a team while dealing with their personal family issues, notably Star-Lord's encounter with his father the ambitious celestial being Ego.",
-    "Language": "English",
-    "Country": "United States",
-    "Awards": "Nominated for 1 Oscar. 15 wins & 59 nominations total",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg",
-    "Ratings": ]);
+	const [movies, setMovies] = useState([]);
 	const [favourites, setFavourites] = useState([]);
 	const [searchValue, setSearchValue] = useState('');
 
